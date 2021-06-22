46482693efccc62dcad3ad0dbe45bcc57c098726
Liteskarr <398501z3@gmail.com>
2021-06-22 11:30:35 +0300

    base fix

--- src/utils/math.py
+++ src/utils/math.py
@@ -2,3 +2,17 @@ def sign(x: int) -> int:
     if not x:
         return x
     return x // abs(x)
+
+
+def activate(value: float,
+             peak: float,
+             left: float = 0,
+             right: float = 1) -> float:
+    if value <= left or value >= right:
+        return 0
+    if value == peak:
+        return 1.0
+    elif peak > value:
+        return value / (peak - left)
+    elif peak < value:
+        return value / (right - peak)
