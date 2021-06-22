c28bdfe50036bb18c9318e52930b1e1754fde4e4
Liteskarr <398501z3@gmail.com>
2021-06-22 12:25:13 +0300

    base fix

--- src/moving_tactics/base.py
+++ src/moving_tactics/base.py
@@ -86,7 +86,7 @@ class BaseMovingTactics(IMovingTactics):
             if ship.Data.Id not in self.states:
                 self.states[ship.Data.Id] = MovingStates.FREE
             if ship.Data.Id not in self.control_points:
-                self.control_points[ship.Data.Id] = list(map(lambda x: (x * (ship.Data.MaxRangeAttack - 1)).__ceil__(),
+                self.control_points[ship.Data.Id] = list(map(lambda x: (x * (ship.Data.MaxRangeAttack - 1)).ceil(),
                                                              CONTROL_POINTS))
 
     def update_enemies_center(self):
